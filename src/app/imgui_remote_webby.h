8920c7941577117055294fa3ea56d47728b775c0
Gargaj <gargaj@conspiracy.hu>
2019-05-13 21:55:54 +0200

    remove stl dependency, use imgui's memory allocator (which may be overridden), clean up whitespaces

--- src/app/imgui_remote_webby.h
+++ src/app/imgui_remote_webby.h
@@ -25,12 +25,12 @@ struct IWebSocketServer;
 
 IWebSocketServer *s_WebSocketServer;
 
-static void onLog         (const char* text);
-static int  onDispatch    (struct WebbyConnection *connection);
-static int  onConnect     (struct WebbyConnection *connection);
-static void onConnected   (struct WebbyConnection *connection);
-static void onDisconnected(struct WebbyConnection *connection);
-static int  onFrame       (struct WebbyConnection *connection, const struct WebbyWsFrame *frame);
+static void onLog( const char* text );
+static int  onDispatch( struct WebbyConnection *connection );
+static int  onConnect( struct WebbyConnection *connection );
+static void onConnected( struct WebbyConnection *connection );
+static void onDisconnected( struct WebbyConnection *connection );
+static int  onFrame( struct WebbyConnection *connection, const struct WebbyWsFrame *frame );
 
 struct IWebSocketServer
 {
@@ -50,22 +50,22 @@ struct IWebSocketServer
 	struct WebbyServerConfig ServerConfig;
 	struct WebbyConnection *Client;
 
-	int Init(const char *local_address, int local_port)
+	int Init( const char *local_address, int local_port )
 	{
 		s_WebSocketServer = this;
 
 #if defined(_WIN32)
 		{
-			WORD wsa_version = MAKEWORD(2,2);
+			WORD wsa_version = MAKEWORD( 2, 2 );
 			WSADATA wsa_data;
-			if (0 != WSAStartup(wsa_version, &wsa_data))
+			if ( 0 != WSAStartup( wsa_version, &wsa_data ) )
 				return -1;
 		}
 #endif
 
-		memset(&ServerConfig, 0, sizeof ServerConfig);
+		memset( &ServerConfig, 0, sizeof ServerConfig );
 		ServerConfig.bind_address = local_address;
-		ServerConfig.listening_port = (unsigned short)(local_port);
+		ServerConfig.listening_port = (unsigned short) ( local_port );
 		ServerConfig.flags = WEBBY_SERVER_WEBSOCKETS;
 		ServerConfig.connection_max = 1;
 		ServerConfig.request_buffer_size = 2048;
@@ -77,11 +77,11 @@ struct IWebSocketServer
 		ServerConfig.ws_closed = &onDisconnected;
 		ServerConfig.ws_frame = &onFrame;
 
-		MemorySize = WebbyServerMemoryNeeded(&ServerConfig);
-		Memory = malloc(MemorySize);
-		Server = WebbyServerInit(&ServerConfig, Memory, MemorySize);
+		MemorySize = WebbyServerMemoryNeeded( &ServerConfig );
+		Memory = malloc( MemorySize );
+		Server = WebbyServerInit( &ServerConfig, Memory, MemorySize );
 		Client = NULL;
-		if (!Server)
+		if ( !Server )
 			return -2;
 
 		return 0;
@@ -89,16 +89,16 @@ struct IWebSocketServer
 
 	void Update()
 	{
-		if (Server)
-			WebbyServerUpdate(Server);
+		if ( Server )
+			WebbyServerUpdate( Server );
 	}
 
 	void Shutdown()
 	{
-		if (Server)
+		if ( Server )
 		{
-			WebbyServerShutdown(Server);
-			free(Memory);
+			WebbyServerShutdown( Server );
+			free( Memory );
 		}
 
 #if defined(_WIN32)
@@ -106,88 +106,88 @@ struct IWebSocketServer
 #endif
 	}
 
-	void WsOnConnected(struct WebbyConnection *connection)
+	void WsOnConnected( struct WebbyConnection *connection )
 	{
 		Client = connection;
 	}
 
-	void WsOnDisconnected(struct WebbyConnection * /*connection*/)
+	void WsOnDisconnected( struct WebbyConnection * /*connection*/ )
 	{
 		Client = NULL;
-		OnMessage(Disconnect, NULL, 0);
+		OnMessage( Disconnect, NULL, 0 );
 	}
 
-	int WsOnFrame(struct WebbyConnection *connection, const struct WebbyWsFrame *frame)
+	int WsOnFrame( struct WebbyConnection *connection, const struct WebbyWsFrame *frame )
 	{
-//		printf("WebSocket frame incoming\n");
-//		printf("  Frame OpCode: %d\n", frame->opcode);
-//		printf("  Final frame?: %s\n", (frame->flags & WEBBY_WSF_FIN) ? "yes" : "no");
-//		printf("  Masked?     : %s\n", (frame->flags & WEBBY_WSF_MASKED) ? "yes" : "no");
-//		printf("  Data Length : %d\n", (int) frame->payload_length);
+		//		printf("WebSocket frame incoming\n");
+		//		printf("  Frame OpCode: %d\n", frame->opcode);
+		//		printf("  Final frame?: %s\n", (frame->flags & WEBBY_WSF_FIN) ? "yes" : "no");
+		//		printf("  Masked?     : %s\n", (frame->flags & WEBBY_WSF_MASKED) ? "yes" : "no");
+		//		printf("  Data Length : %d\n", (int) frame->payload_length);
 
-		std::vector<unsigned char> buffer(frame->payload_length+1);
-		WebbyRead(connection, &buffer[0], frame->payload_length);
-		buffer[frame->payload_length] = 0;
-//        if(!strstr((char*)&buffer[0],"ImMouseMove"))
-//            printf("  Data : %s\n", &buffer[0]);
+		Vector<unsigned char> buffer( frame->payload_length + 1 );
+		WebbyRead( connection, &buffer[ 0 ], frame->payload_length );
+		buffer[ frame->payload_length ] = 0;
+		//        if(!strstr((char*)&buffer[0],"ImMouseMove"))
+		//            printf("  Data : %s\n", &buffer[0]);
 
-		OnMessage((OpCode)frame->opcode, &buffer[0], frame->payload_length);
+		OnMessage( (OpCode) frame->opcode, &buffer[ 0 ], frame->payload_length );
 
 		return 0;
 	}
 
-	virtual void OnMessage(OpCode /*opcode*/, const void * /*data*/, int /*size*/) { }
+	virtual void OnMessage( OpCode /*opcode*/, const void * /*data*/, int /*size*/ ) { }
 	virtual void OnError() { }
 
-	virtual void SendText(const void *data, int size)
+	virtual void SendText( const void *data, int size )
 	{
-		if (Client)
+		if ( Client )
 		{
-			WebbySendFrame(Client, WEBBY_WS_OP_TEXT_FRAME, data, size);
+			WebbySendFrame( Client, WEBBY_WS_OP_TEXT_FRAME, data, size );
 		}
 	}
 
-	virtual void SendBinary(const void *data, int size)
+	virtual void SendBinary( const void *data, int size )
 	{
-		if (Client)
+		if ( Client )
 		{
-			WebbySendFrame(Client, WEBBY_WS_OP_BINARY_FRAME, data, size);
+			WebbySendFrame( Client, WEBBY_WS_OP_BINARY_FRAME, data, size );
 		}
 	}
 };
 
 
-static void onLog(const char* /*text*/)
+static void onLog( const char* /*text*/ )
 {
 	//printf("[WsOnLog] %s\n", text);
 }
 
-static int onDispatch(struct WebbyConnection * /*connection*/)
+static int onDispatch( struct WebbyConnection * /*connection*/ )
 {
 	//printf("[WsOnDispatch] %s\n", connection->request.uri);
 	return 1;
 }
 
-static int onConnect(struct WebbyConnection * /*connection*/)
+static int onConnect( struct WebbyConnection * /*connection*/ )
 {
 	//printf("[WsOnConnect] %s\n", connection->request.uri);
 	return 0;
 }
 
-static void onConnected(struct WebbyConnection *connection)
+static void onConnected( struct WebbyConnection *connection )
 {
 	//printf("[WsOnConnected]\n");
-	s_WebSocketServer->WsOnConnected(connection);
+	s_WebSocketServer->WsOnConnected( connection );
 }
 
-static void onDisconnected(struct WebbyConnection *connection)
+static void onDisconnected( struct WebbyConnection *connection )
 {
 	//printf("[WsOnDisconnected]\n");
-	s_WebSocketServer->WsOnDisconnected(connection);
+	s_WebSocketServer->WsOnDisconnected( connection );
 }
 
-static int onFrame(struct WebbyConnection *connection, const struct WebbyWsFrame *frame)
+static int onFrame( struct WebbyConnection *connection, const struct WebbyWsFrame *frame )
 {
 	//printf("[WsOnFrame]\n");
-	return s_WebSocketServer->WsOnFrame(connection, frame);
+	return s_WebSocketServer->WsOnFrame( connection, frame );
 }
